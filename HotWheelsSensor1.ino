bf2f28661f6b1fc74fc8b53fb21f0292a467e3c1
tom <tomjuggler@gmail.com>
2020-05-13 21:28:36 +0200

    VS mode! First car wins reset with Serial.read() inByte
    Signed-off-by: tom <tomjuggler@gmail.com>

--- HotWheelsSensor1.ino
+++ HotWheelsSensor1.ino
@@ -1,30 +1,67 @@
-//add another sensor and we have it!!!!
+//Processing serial code with traffic light start..
+//Two sensors on Arduino, two tracks. First sensor triggered indicates winner
+//Processing App detects serial from winning sensor and Checkered flag ensues, also "Winner, lane one!" or whatever
+//some way to reset (two way communication?)
+//start over!
 
 const int ledPin = 2;
+const int ledPin2 = 3;
 const int ldrPin = A0;
+const int ldrPin2 = A1;
 int num = 0;
 long currentMillis = 0;
 long previousMillis = 0;
+long previousMillis2 = 0;
+
+int inByte = 0;
+boolean ready = false;
 
 void setup() {
   Serial.begin(115200);
   pinMode(ldrPin, INPUT);
+  pinMode(ldrPin2, INPUT);
+
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
   digitalWrite(ledPin, HIGH);
+  digitalWrite(ledPin2, HIGH);
 
 
 }
 
 void loop() {
+  if (Serial.available() > 0) {
+    // get incoming byte:
+    inByte = Serial.read();
+    //todo: check which byte! - doesn't really matter.. maybe toggle speed mode and VS mode!
+    ready = true;
+  }
+  if(ready){
   int ldrStatus = analogRead(ldrPin);
-  //  Serial.println(ldrStatus);
-  if (ldrStatus < 8) {
-    if (millis() - previousMillis > 100) {
-      Serial.print("CAR!! ");
-      Serial.println(num++);
-      Serial.println(millis()-previousMillis); //time from one lap to next!
-    }
-    previousMillis = millis();
+  int ldrStatus2 = analogRead(ldrPin2);
+//      Serial.println(ldrStatus); //testing
+
+  if (ldrStatus < 12) {
+//    if (millis() - previousMillis > 100) {
+      Serial.println("lane ONE! "); // one or two, not both
+//      Serial.println(num++);
+//      Serial.println(millis() - previousMillis); //time from one lap to next!
+//      currentMillis = millis();
+//    }
+//    previousMillis = millis();
+    ready = false; //reset
+  }
+//Serial.println(ldrStatus2); //testing
+  if (ldrStatus2 < 17) {
+//    if (millis() - previousMillis2 > 100) {
+      Serial.println("lane TWO! "); //one or two, not both
+//      Serial.println(num++);
+//      Serial.print("TOTAL TIME: ");
+//      Serial.println(millis() - currentMillis);
+//    }
+//    previousMillis2 = millis();
+    ready = false; //reset
+  }
   }
 
 }
